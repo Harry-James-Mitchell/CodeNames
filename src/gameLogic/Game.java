e57cfb3ee970f5c0479c52d8d72e3947f39a79a4
Harry Mitchell <harrym@iastate.edu>
2018-07-01 18:50:45 -0500

    I forgot to save game.java before my last commit.

--- src/gameLogic/Game.java
+++ src/gameLogic/Game.java
@@ -21,8 +21,19 @@ public class Game extends JFrame {
 	private final int height = 700;
 	private static long seed;
 	private final int NUMBER_OF_CARDS = 25;
+	private final int RED_CARDS = 9;
+	private final int BLUE_CARDS = 8;
+	private final int DEATH_CARDS = 1;
 	
 	public Game(){
+		initUI();
+	}
+	
+	private void newGame() {
+		
+	}
+	
+	private void initUI() {
 		JPanel displayPanel = new JPanel();
 		setTitle("CodeNames");
 		setSize(width, height);
@@ -32,7 +43,7 @@ public class Game extends JFrame {
         File file = new File("textFiles/Words.txt");//This text file contains all of the words used in the game
         Random rand = new Random(seed); //We use the room code as the seed so everyone has the same board.
         
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<String>(); //This arraylist will contain all of the words used in the game
         try {
         	Scanner sc = new Scanner(file);
         	while(sc.hasNextLine()) {
@@ -46,12 +57,29 @@ public class Game extends JFrame {
         	MainMenu.main(args);
         }
         
-        JButton[] cards = new JButton[NUMBER_OF_CARDS];
+        //assign a word to every button and keep track of them using the codeNameCard object
+        JButton[] cardButtons = new JButton[NUMBER_OF_CARDS];
+        CodeNameCard[] cnc = new CodeNameCard[NUMBER_OF_CARDS];
+        ArrayList<Integer> locations = new ArrayList<Integer>();
         for(int i=0; i<NUMBER_OF_CARDS; i++){
-        	
+        	int index = rand.nextInt(words.size());
+        	String nextWord = words.get(index);
+        	words.remove(index);
+        	cardButtons[i] = new JButton(nextWord);
+        	cnc[i] = new CodeNameCard(nextWord, "white", i);
+        	locations.add(i);
         }
         
+        //Assign all of red's cards
+        for(int i=0; i<RED_CARDS; i++) {
+        	int index = rand.nextInt(locations.size());
+        	cnc[index].setColor("red");
+        	locations.remove(index);
+        }
         
+        for(int i=0;i<NUMBER_OF_CARDS; i++) {
+        	displayPanel.add(cardButtons[i]);
+        }
         this.add(displayPanel);
         this.setVisible(true);
 	}
