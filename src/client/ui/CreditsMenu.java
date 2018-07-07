7282edfeb9a3d12f75c16e860284c8a93b9cc604
jjb1 <jjb1@iastate.edu>
2018-07-06 21:56:44 -0500

    added bare bones to credits

--- src/client/ui/CreditsMenu.java
+++ src/client/ui/CreditsMenu.java
@@ -1,10 +1,93 @@
 package client.ui;
 
-public class CreditsMenu {
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CreditsMenu extends JFrame{
 	
 	//todo to add jerry
 
-public static void main(String args[]) {
+	private int width = 400;
+	private int height = 400;
+	
+	//Jerry: programming
+	//Harry: programming
+	//Josh: programming
+	//Noah: art/programming
+	
+	
+	public CreditsMenu()
+	{
+		initUI();
+	}
+	
+	public void initUI()
+	{
+		
+		JPanel displayPanel = new JPanel();
+		displayPanel.setLayout(null);
+		setTitle("Credits");
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		JLabel jer = new JLabel("Jeremiah Brusegaard");
+		jer.setLocation(50, 50);
+		jer.setSize(150, 20);
+		
+		JLabel jos = new JLabel("Josh Brenneman");
+		jos.setLocation(250, 50);
+		jos.setSize(150, 20);
+		
+		JLabel har = new JLabel("Harry Mitchell");
+		har.setLocation(50, 150);
+		har.setSize(150, 20);
+		
+		JLabel noa = new JLabel("Noah Mitchell");
+		noa.setLocation(250, 150);
+		noa.setSize(150, 20);
+		
+		JButton mainBtn = new JButton("Main Menu");
+		
+		mainBtn.addActionListener((ActionEvent event) -> {
+			MainMenu.main(null);
+			leavePage();
+		});
+		
+		
+		jer.setLocation(50, 50);
+		jer.setSize(150, 20);
+		
+		mainBtn.setLocation(150, 300);
+		mainBtn.setSize(100, 50);
+		
+		displayPanel.add(jer);
+		displayPanel.add(jos);
+		displayPanel.add(har);
+		displayPanel.add(noa);
+		displayPanel.add(mainBtn);
+		
+        this.add(displayPanel);
+        this.setVisible(true);
+	}
+	
+	private void leavePage() {
+		this.removeAll();
+		this.setVisible(false);
+	}
+	
+	public static void main(String args[]) {
+		
+		EventQueue.invokeLater(() -> {
+            CreditsMenu cm = new CreditsMenu();
+            cm.setVisible(true);
+        });
 		
 	}
 }
