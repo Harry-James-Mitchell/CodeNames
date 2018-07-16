9c9f792ddf917d9290f11b1c34f5390003cd9e62
Harry Mitchell <harrym@iastate.edu>
2018-07-15 22:19:22 -0500

    started fixing the credits page to make it look nicer.

--- src/client/ui/CreditsMenu.java
+++ src/client/ui/CreditsMenu.java
@@ -10,16 +10,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CreditsMenu extends JFrame{
-	
-	//todo to add jerry
 
-	private int width = 400;
-	private int height = 400;
+	private final int WIDTH = 400;
+	private final int HEIGHT = 500;
+	private final int PIC_WIDTH = 150;
+	private final int PIC_HEIGHT = 150;
 	
-	//Jerry: programming
-	//Harry: programming
-	//Josh: programming
-	//Noah: art/programming
+	//Harry: lead dev
+	//Jerry: lead backend dev
+	//Josh: lead frontend dev
+	//Noah: lead artist
 	
 	
 	public CreditsMenu()
@@ -29,44 +29,47 @@ public class CreditsMenu extends JFrame{
 	
 	public void initUI()
 	{
-		
 		JPanel displayPanel = new JPanel();
 		displayPanel.setLayout(null);
 		setTitle("Credits");
-		setSize(width, height);
+		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		ImageIcon icon = new ImageIcon("pics/Frame.png");
         setIconImage(icon.getImage());
 		
+        ImageIcon JerryFace = new ImageIcon("pics/JerryFace.png");
+        ImageIcon JoshFace = new ImageIcon("pics/Josh_Face.png");
+        ImageIcon HarryFace = new ImageIcon("pics/Dab.png");
+        ImageIcon NoahFace = new ImageIcon("pics/NoahMeme.png");
 		
 		JLabel jer = new JLabel("Jeremiah Brusegaard");
-		jer.setLocation(50, 50);
+		jer.setLocation(50, 150);
 		jer.setSize(150, 20);
+		JLabel JerryPic = new JLabel(JerryFace);
+		JerryPic.setSize(PIC_WIDTH, PIC_HEIGHT);
+//		JerryPic.setLocation(x, y);
 		
 		JLabel jos = new JLabel("Josh Brenneman");
 		jos.setLocation(250, 50);
 		jos.setSize(150, 20);
 		
 		JLabel har = new JLabel("Harry Mitchell");
-		har.setLocation(50, 150);
+		har.setLocation(65, 10);
 		har.setSize(150, 20);
+		JLabel HarryPic = new JLabel(HarryFace);
+		HarryPic.setSize(PIC_WIDTH, PIC_HEIGHT);
+		HarryPic.setLocation(25, 40);
 		
 		JLabel noa = new JLabel("Noah Mitchell");
 		noa.setLocation(250, 150);
 		noa.setSize(150, 20);
 		
 		JButton mainBtn = new JButton("Main Menu");
-		
 		mainBtn.addActionListener((ActionEvent event) -> {
 			MainMenu.main(null);
 			leavePage();
-		});
-		
-		
-		jer.setLocation(50, 50);
-		jer.setSize(150, 20);
-		
+		});		
 		mainBtn.setLocation(150, 300);
 		mainBtn.setSize(100, 50);
 		
@@ -75,6 +78,7 @@ public class CreditsMenu extends JFrame{
 		displayPanel.add(har);
 		displayPanel.add(noa);
 		displayPanel.add(mainBtn);
+		displayPanel.add(HarryPic);
 		
         this.add(displayPanel);
         this.setVisible(true);
